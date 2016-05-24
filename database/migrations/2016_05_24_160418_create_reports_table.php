15856fda90211037abe981b5c1d878c662008771
sultann <manikdrmc@gmail.com>
2016-05-24 22:56:58 +0600

    migration view bootstap seeder added

---
+++ database/migrations/2016_05_24_160418_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('task_done', 500);
+            $table->text('blocker', 250);
+            $table->boolean('blocker_status')->default(1);
+            $table->boolean('can_update')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams');
+    }
+}
